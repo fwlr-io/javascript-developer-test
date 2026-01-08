4773ce5f96aa8624d236a95b77531c5b4aed890d
scottfowler <scott@fwlr.io>
2026-01-08 17:15:13 +1100

    initial solve, needs Promise.all

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,19 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = [];
+
+  for (const url of urls) {
+    const { status, body } = await httpGet(url);
+    const { message } = JSON.parse(body);
+    if (status == 200) {
+      results.push({ "Arnie Quote": message });
+    } else {
+      results.push({ FAILURE: message });
+    }
+  }
+
+  return results;
 };
 
 module.exports = {
