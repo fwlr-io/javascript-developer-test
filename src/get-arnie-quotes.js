c96fb77cc6ae9c5871b7f07f83e12159cdef0e7a
scottfowler <scott@fwlr.io>
2026-01-08 17:20:11 +1100

    inline inner function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,16 +1,17 @@
 const { httpGet } = require("./mock-http-interface");
 
-const getArnieQuote = async (url) => {
-  const { status, body } = await httpGet(url);
-  const { message } = JSON.parse(body);
-  if (status == 200) {
-    return { "Arnie Quote": message };
-  } else {
-    return { FAILURE: message };
-  }
-};
-
-const getArnieQuotes = async (urls) => Promise.all(urls.map(getArnieQuote));
+const getArnieQuotes = async (urls) =>
+  Promise.all(
+    urls.map(async (url) => {
+      const { status, body } = await httpGet(url);
+      const { message } = JSON.parse(body);
+      if (status == 200) {
+        return { "Arnie Quote": message };
+      } else {
+        return { FAILURE: message };
+      }
+    }),
+  );
 
 module.exports = {
   getArnieQuotes,
