ba20226b3742bd5e2af1c2dba311b1c5a1c612a7
scottfowler <scott@fwlr.io>
2026-01-08 17:24:59 +1100

    refactor to be shorter

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -4,12 +4,17 @@ const getArnieQuotes = async (urls) =>
   Promise.all(
     urls.map(async (url) => {
       const { status, body } = await httpGet(url);
-      const { message } = JSON.parse(body);
-      if (status == 200) {
-        return { "Arnie Quote": message };
-      } else {
-        return { FAILURE: message };
-      }
+      return {
+        [status == 200 ? "Arnie Quote" : "FAILURE"]: JSON.parse(body).message,
+      };
+
+      // const { status, body } = await httpGet(url);
+      // const { message } = JSON.parse(body);
+      // if (status == 200) {
+      //   return { "Arnie Quote": message };
+      // } else {
+      //   return { FAILURE: message };
+      // }
     }),
   );
 
