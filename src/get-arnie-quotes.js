47cb87aa62a7316d6a729e6e79566864ea72ecde
scottfowler <scott@fwlr.io>
2026-01-08 17:26:00 +1100

    revert refactor, readability > terseness

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -4,17 +4,12 @@ const getArnieQuotes = async (urls) =>
   Promise.all(
     urls.map(async (url) => {
       const { status, body } = await httpGet(url);
-      return {
-        [status == 200 ? "Arnie Quote" : "FAILURE"]: JSON.parse(body).message,
-      };
-
-      // const { status, body } = await httpGet(url);
-      // const { message } = JSON.parse(body);
-      // if (status == 200) {
-      //   return { "Arnie Quote": message };
-      // } else {
-      //   return { FAILURE: message };
-      // }
+      const { message } = JSON.parse(body);
+      if (status == 200) {
+        return { "Arnie Quote": message };
+      } else {
+        return { FAILURE: message };
+      }
     }),
   );
 
